07e18343d82678e542a1b014ddecdd09eabf2b07
ohprettyhak <me@haklee.me>
2025-08-07 14:52:17 +0900

    chore(deps): update dependencies in package.json and add SpeedInsights component

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import Script from 'next/script';
@@ -65,6 +66,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
           <NavigationMenu />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
 
       <Script
